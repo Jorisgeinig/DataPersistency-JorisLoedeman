77597d76aefcbf1f06b30566f167e7e251319184
jorisgeinig <joris1300@live.nl>
2023-10-22 17:57:54 +0200

    S6 finished

--- opdracht/S6_Views_student.sql
+++ opdracht/S6_Views_student.sql
@@ -16,8 +16,24 @@
 --     CREATE OR REPLACE VIEW personeel AS
 -- 	     SELECT mnr, voorl, naam as medewerker, afd, functie
 --       FROM medewerkers;
--- 3. Is de view "deelnemers" updatable ? Waarom ?
 
+CREATE OR REPLACE VIEW deelnemers AS
+select inschrijvingen.cursist, inschrijvingen.cursus, inschrijvingen.begindatum,
+       uitvoeringen.docent, uitvoeringen.locatie
+from inschrijvingen
+         join uitvoeringen on uitvoeringen.cursus = inschrijvingen.cursus and
+                              uitvoeringen.begindatum = inschrijvingen.begindatum;
+
+
+CREATE OR REPLACE VIEW personeel AS
+SELECT mnr, voorl, naam as medewerker, afd, functie from medewerkers;
+
+
+select * from deelnemers
+                  join personeel on deelnemers.cursist = personeel.mnr
+
+-- 3. Is de view "deelnemers" updatable ? Waarom ?
+-- Ja, want de view eenvoudige informatie  bevat van de twee tabellen en omdat er primary keys aanwezig zijn.
 
 -- S6.2.
 --
@@ -27,3 +43,31 @@
 --    Deze view dient de uitvoeringsgegevens op te halen voor de "dagcurssussen" (gebruik ook de view "dagcursussen"). Toon aan dat de view werkt
 -- 3. Verwijder de views en laat zien wat de verschillen zijn bij DROP view <viewnaam> CASCADE en bij DROP view <viewnaam> RESTRICT
 
+create or replace view dagcursussen as
+select cu.code, cu.omschrijving, cu.type from cursussen cu
+where cu.lengte = 1;
+
+select * from dagcursussen;
+
+create or replace view daguitvoeringen as
+select * from dagcursussen
+                  join uitvoeringen u on dagcursussen.code = u.cursus;
+
+select * from daguitvoeringen;
+
+
+DROP VIEW dagcursussen cascade;
+--Beide views worden verwijdert
+
+drop view dagcursussen restrict;
+--cannot drop view dagcursussen because other objects depend on it
+
+drop view daguitvoeringen cascade;
+--alleen view daguitvoeringen wordt gewist
+
+drop view daguitvoeringen restrict;
+--daguitvoeringen wordt gewist
+
+--view daguitvoeringen kan dus los worden gewist omdat andere views/objecten niet afhankelijk zijn van de view
+--integendeel tot dagcursussen, daar is daguitvoeringen van afhankelijk dus wordt die view ook gewist bij cascade,
+--of het weigert te verwijderen bij de commando restrict door de afhankelijkheid.
